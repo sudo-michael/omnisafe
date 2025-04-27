8e7a5e5882440a0d3be86b135b4beb5e7baab4a4
mla233 <mla233@cs-star.cmpt.sfu.ca>
2025-04-27 01:29:48 -0700

    bac

---
+++ exp-x/omnisafe_test_benchmark_navigation/SafetyCarGoal0-v0---87d773fd7ed040f17185eb1361e99a41f2e6e72f0ad49dffad66aae2415063f0/CPPOPID-{SafetyCarGoal0-v0}/seed-002-2025-04-23-08-30-01/wandb/run-20250423_083003-3q7jkgvn/files/code/experiment_scripts/test.py
@@ -0,0 +1,45 @@
+import warnings
+
+import torch
+
+from omnisafe.common.experiment_grid import ExperimentGrid
+from omnisafe.utils.exp_grid_tools import train
+
+
+if __name__ == '__main__':
+    experiment_name = 'omnisafe_test_benchmark'
+    eg = ExperimentGrid(exp_name='omnisafe_test_benchmark_navigation')
+
+    # Set the algorithms.
+    naive_lagrange_policy = ['PPOLag', 'CPPOPID']
+    penalty_policy = ['P3O', 'IPO']
+
+    # Set the environments.
+    # mujoco_envs = [
+    #     'SafetyAntVelocity-v1',
+    #     'SafetyHopperVelocity-v1',
+    #     # 'SafetyHumanoidVelocity-v1',
+    #     # 'SafetyWalker2dVelocity-v1',
+    #     'SafetyHalfCheetahVelocity-v1',
+    #     # 'SafetySwimmerVelocity-v1',
+    # ]
+    # eg.add('env_id', mujoco_envs)
+    navigation_envs = [
+        'SafetyCarGoal0-v0',
+        'SafetyCarGoal1-v0',
+        'SafetyCarCircle0-v0',
+        'SafetyCarCircle1-v0',
+    ]
+    eg.add('env_id', navigation_envs)
+
+
+    eg.add('algo', naive_lagrange_policy + penalty_policy)
+    eg.add('train_cfgs:device', "cuda")
+    eg.add('train_cfgs:vector_env_nums', [4])
+    eg.add('train_cfgs:torch_threads', [8])
+    eg.add('train_cfgs:total_steps', [10_000_000])
+    eg.add('algo_cfgs:steps_per_epoch', [20_000])
+    eg.add('logger_cfgs:use_wandb', [True])
+    eg.add('logger_cfgs:use_tensorboard', [False])
+    eg.add('seed', [0, 1, 2, 3, 4])
+    eg.run(train, num_pool=8, gpu_id=[0, 1])
